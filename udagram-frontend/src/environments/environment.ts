74bf076e986e873b93e88be22147eec3d9d0e65d
Mariemfakhreldein <mariem.mfe@gmmail.com>
2021-09-25 18:29:40 +0200

    frontend exposed

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4fcee532c02f4709acb5dfaf9ab997d-846831285.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
